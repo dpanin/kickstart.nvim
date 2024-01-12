1a45ac0ad4947bbfe16d7c3c149abfe13a797ead
Denis Panin <admolis@Deniss-MacBook-Pro.local>
2024-01-12 09:23:43 +0600

    Add nvim-tree + change color scheme

--- init.lua
+++ init.lua
@@ -883,6 +883,7 @@ require('lazy').setup({
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
+    opts = { style = "moon" },
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
